6c09bdfc0bfbc242680b4dae1c23548f03744eb6
Den <dzianis.smalonski@protofire.io>
2025-07-24 16:11:40 +0200

    refactor: remove unused extend_schema decorator from analytics views

--- safe_transaction_service/analytics/views_v2.py
+++ safe_transaction_service/analytics/views_v2.py
@@ -3,7 +3,6 @@ from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
-from drf_spectacular.utils import extend_schema
 
 from safe_transaction_service.analytics.services.analytics_service import (
     get_analytics_service,
@@ -17,7 +16,6 @@ class AnalyticsMultisigTxsByOriginListView(ListAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    @extend_schema(exclude=True)
     def get(self, request, format=None):
         analytics_service = get_analytics_service()
         return Response(analytics_service.get_safe_transactions_per_safe_app())
@@ -44,7 +42,6 @@ class AnalyticsSafeStatisticsView(ListAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    @extend_schema(exclude=True)
     def get(self, request, format=None):
         analytics_service = get_analytics_service()
         return Response(analytics_service.get_safe_statistics())
